c3c7b3d495e1ca2c4c2bd6a97ba7f6689b734d45
Alex Sutton <alexsutton14@hotmail.com>
2019-09-03 12:00:46 +0100

    Added dependent destroy to tag.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,7 +7,10 @@ class TagsController < ApplicationController
         @tag = Tag.find(params[:id])
     end
     def destroy
-        
+        tag = Tag.find(params[:id])
+        tag.destroy
+        flash.notice = "Tag '#{@tag.name}' Deleted!"
+        redirect_to tags_path
     end
 
 end
